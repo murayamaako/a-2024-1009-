a0944c968fc9579840faffc50ab45d006b98d492
murayamaako <167832805+murayamaako@users.noreply.github.com>
2024-10-29 10:37:59 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,10 +1,5 @@
 var who65='world';
-function sayhello66(){
-alert('Hello,'+who66+'!');
-}
-function taro66(){
-  who66='太郎が来た';
-}
-function hanako66(){
-  who66='花子が来た';
-}
+function sayhello66(){alert('Hello,'+who66+'!');}
+function taro66(){who66='太郎';}
+function hanako66(){who66='花子';}
+function gakita66(){alert(who66,'+が来た');}
