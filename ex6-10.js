5066a6fc315b48f9293abb375d02236928d925ae
murayamaako <167832805+murayamaako@users.noreply.github.com>
2024-11-26 10:32:11 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -2,7 +2,7 @@ function imgchange1() {
     document.getElementById('logo').src = './image/heartmoji_06.png';
 }
 function imgchange2() {
-    document.getElementById('logo').src = './image/unnamed.jpg';
+    document.getElementById('logo').src = './image/heartmoji_08.png';
 }
 
 
