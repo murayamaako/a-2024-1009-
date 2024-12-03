1c8608ab2b54cda92c6fb705d134694e8c71a196
murayamaako <167832805+murayamaako@users.noreply.github.com>
2024-12-03 10:45:23 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -2,7 +2,7 @@ function hyoji(){
 //表示する文字
 var str="一文字ずつ表示します。";
 //テキストボックスの文字数
-var cnt="document.timer.moji.value.length;
+var cnt=document.timer.moji.value.length;
 //文字が全部表示されるか確認
 if( cnt < 11 ){
 //現在よりも1文字多く切り出して表示
