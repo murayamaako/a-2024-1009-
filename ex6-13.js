c10f8f51edd9876c1df2273db395b2c8de09d3b3
murayamaako <167832805+murayamaako@users.noreply.github.com>
2024-12-09 15:56:23 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,14 +1,1 @@
-function mes(){alert("3秒立ちました！");}
-function hyoji13(){
-  //表示する文字
-var str13="一文字ずつ表示します。"}
-//テキストボックスの文字数
-var cnt13=docment.timer13.moji13.value.length;
-//文字数が全部表示さえrているか確認
-if(cnt13<11){
-  //現在より１文字多く切り出して表示
-document.timer13.moji13.value=str13.subsutr(0,cnt13+1);}eles{
-//全て表示されたら、空文字に戻す
-document.timer.13.moji13.value="";}
-//setTimeout()を含む関数を呼び出す
-setTimeout("hyoji13()",1000):}
+function mes(){alert("3秒経ちました！");}
