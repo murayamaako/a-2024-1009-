0fc75f138d65c285a48f223ab73bf395d16f33c2
murayamaako <167832805+murayamaako@users.noreply.github.com>
2024-12-10 09:29:12 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
-function imgchange5(){document.getElementById('moana').src='./image/0.jpg';}
-function mes2(){document.getElementById('moana').src='./image/1.jpg';}
+function imgchange5(){document.getElementById('moana').src='./image/1.jpg';}
+function mes2(){document.getElementById('moana').src='./image/0.jpg';}
 
