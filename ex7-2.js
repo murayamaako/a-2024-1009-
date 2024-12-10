cec520d4b316665285c7cc9471c40cf8b94824f7
murayamaako <167832805+murayamaako@users.noreply.github.com>
2024-12-10 10:41:38 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,4 +1,4 @@
 function add(){
-  var x = document.getElementByid('input1').value;
-  var y = document.getElementByid('input2').value;
+  var x = document.getElementByld('input1').value;
+  var y = document.getElementByld('input2').value;
   alert(x+y);}
